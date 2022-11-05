cc4446872b6c2d3723e3af1b0a13be55fbd7e891
Sayem92 <sayemmiha123@gmail.com>
2022-11-05 20:47:49 +0600

    add pdf download

--- src/components/TuitorialAll/Tutorials/DetailsPage/DetailsPage.js
+++ src/components/TuitorialAll/Tutorials/DetailsPage/DetailsPage.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import Pdf from "react-to-pdf";
+
+const ref = React.createRef();
 
 const DetailsPage = () => {
     const details = useLoaderData();
     const { id, image, title, description, tips } = details;
 
+
     return (
 
-        <div className="lg:w-9/12 lg:p-4 mx-auto lg:my-10 pb-5  bg-gray-300">
-            <div className="  ">
+        <div ref={ref} className="lg:w-9/12 lg:p-4 mx-auto lg:my-10 pb-5  bg-gray-300">
+            <div  className="  ">
                 <div className='flex justify-between items-center'>
                     <h2 className=" py-4 pl-6  font-bold text-3xl text-gray-900">{title}</h2>
                     <div className='mr-5'>
-                        <Link to={''}>
-                            <button className="btn btn-success hover:bg-green-600">Pdf</button>
-                        </Link>
+
+                        <Pdf targetRef={ref}  x={3} y={1} scale={0.7} filename="tutorial-example.pdf">
+
+                            {({ toPdf }) => <button
+                             onClick={toPdf} 
+                             className="btn btn-success hover:bg-green-600">Pdf</button>}
+                        </Pdf>
                     </div>
                 </div>
                 <div>
