a18fa250ca7cf1a91bfafd96e342e021304d2159
Sayem92 <sayemmiha123@gmail.com>
2023-02-05 14:53:12 +0600

    add pricing section

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
                     <div className="max-w-md ">
                         <h1 className="mb-5 text-5xl font-bold">Welcome to <span className='text-orange-500'>The Smart Tutor</span> </h1>
                         <p className="mb-5"> A tutorial is a teaching session given to one student or a small group of students. If you are stuck in a class, you could use a tutorial. Teachers lead classes, but tutors are educators who also help students, usually one-on-one. Tutors don't give grades: they just try to help or coach students.</p>
-                        <Link to='/tutorials'> <button className="btn btn-primary">Get Started</button></Link>
+                        <Link to='/tutorials'> <button className="btn bg-gradient-to-r from-green-500 to-blue-500 rounded-md">Get Started</button></Link>
                     </div>
                 </div>
             </div>
